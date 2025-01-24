f2e90d8456f57e9ce1ef1bff325df250e4c0b9d0
Mahak Patel <mahakpateluiux@gmail.com>
2025-01-24 18:03:06 -0500

    Final Submission File.

--- Week 3/Assign1-MahakPatel/JS_Assign-1-groupLogin.js
+++ Week 3/Assign1-MahakPatel/JS_Assign-1-groupLogin.js
@@ -3,17 +3,17 @@ var teamNumber = 3; // User can keep anything in between 1 to 6
 var teamMembers = {
     "Priyank": "Priyank Shah",
     "Rutul": "Rutul Manani",
-    "John": "John Doe",
-    "Mahak": "Mahak Patel"
+    "Smit": "Smit Patel",
+    "Samay": "Samay Raina"
 };
 
 // Ask the user for their team number
 var userInputTeamNumber = prompt("Enter your team number:");
 
 if (isNaN(userInputTeamNumber)) {
-    alert("Invalid input: Please enter a number.");
+    alert("Access denied!");
 } else if (userInputTeamNumber != teamNumber) {
-    alert("Access denied: Incorrect team number.");
+    alert("Access denied!");
 } else {
     // If correct, ask for the user's first name
     var userFirstName = prompt("Enter your first name:");
