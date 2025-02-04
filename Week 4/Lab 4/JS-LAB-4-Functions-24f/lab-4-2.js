9c373b9079511af41d42ab154f2952e9e1000869
Mahak Patel <mahakpateluiux@gmail.com>
2025-02-04 14:56:38 -0500

    Lab 4 submitted work.

---
+++ Week 4/Lab 4/JS-LAB-4-Functions-24f/lab-4-2.js
@@ -0,0 +1,27 @@
+﻿//#### LAB 4 - FUNCTIONS ####
+//PART 2: AN AVERAGE FUNCTION
+
+//################## CREATE YOUR AVERAGE FUNCTION
+// This function takes five numbers and returns their average to one decimal place.
+function averageIt(num1, num2, num3, num4, num5) {
+    return ((num1 + num2 + num3 + num4 + num5) / 5).toFixed(1);
+}
+var getNum = averageIt(5, 10, 15, 20, 25);
+alert("The average is: " + getNum);
+
+//################## LOGIC THAT OUTPUTS MESSAGES BASED ON FUNCTION RESULTS
+
+var course1 = 8; 
+var course2 = 7; 
+var course3 = 9; 
+var course4 = 6; 
+var course5 = 70;
+
+var overallAverage = averageIt(course1, course2, course3, course4, course5);
+alert("Your program average is: " + overallAverage);
+
+if (overallAverage >= 70) {
+    alert("Success! You passed with an average of " + overallAverage);
+} else {
+    alert("Review required! Your average is " + overallAverage);
+}
---
+++ Week 4/Lab 4/JS-LAB-4-Functions-24f/lab-4-3.js
@@ -0,0 +1,26 @@
+﻿//#### LAB 4 - FUNCTIONS ####  
+//PART 3: SAFE DOG WALKING CHECK  
+
+//================== CREATE YOUR checkTemp FUNCTION  
+// This function checks if the temperature is safe for walking.  
+// It needs to receive currentTemp (number).  
+// It will return true if too hot or too cold, otherwise false.  
+
+function checkTemp(currentTemp) {
+    if (currentTemp >= 30 || currentTemp <= -10) {
+        return true;  // Temperature is extreme, return true
+    } else {
+        return false; // Temperature is safe, return false
+    }
+}
+
+//================== GET USER INPUT  
+var userTemp = prompt("Enter your current temperature:");
+userTemp = parseFloat(userTemp); // Convert user input to a number
+
+//================== LOGIC THAT OUTPUTS MESSAGES BASED ON FUNCTION RESULTS  
+if (checkTemp(userTemp) === true) {
+    alert("Yikes! This is no weather for dog walking!");
+} else {
+    alert("You're good, have a nice walk!");
+}
