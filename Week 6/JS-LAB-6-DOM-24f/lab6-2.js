769103bb8d1c28db97e666f09ccef7788fef643f
Mahak Patel <mahakpateluiux@gmail.com>
2025-02-18 14:30:27 -0500

    Lab 6 final commit.

---
+++ Week 6/JS-LAB-6-DOM-24f/lab6-2.js
@@ -0,0 +1,34 @@
+﻿//#### LAB 6 - DOM MANIPULATION ####
+//PART 2: MYSTERY BOX
+
+//LISTEN FOR load EVENT
+window.onload = pageReady;
+
+//'WRAPPER' FUNCTION FOR DOM LOGIC
+function pageReady() {
+    // GET DOM ELEMENTS WE'LL NEED
+    var mysteryBox = document.getElementById("mysteryBox");
+    var buttonBox = document.getElementById("buttonBox");
+    
+    // FUNCTION TO ASK USER
+    function showPrompt() {
+		var result = confirm("Do you want to see something?");
+		
+		if(result === true){
+			mysteryBox.style.display = "none";
+            buttonBox.style.display = "block";
+		}
+	
+    }
+    
+    // FUNCTION TO CHANGE buttonBox
+    function changeButtonBox() {
+        buttonBox.style.width = "600px";
+        buttonBox.style.backgroundColor = "orange";
+		buttonBox.innerHTML = "<h2>SURPRISE!!</h2>";
+    }
+    
+    // SETUP LISTENERS
+    mysteryBox.onmouseover = showPrompt;
+    buttonBox.onclick = changeButtonBox;
+}
