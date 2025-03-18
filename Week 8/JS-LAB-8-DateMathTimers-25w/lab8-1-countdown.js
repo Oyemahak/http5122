cb3274c657a54ffbc8b4392d4342032ef9ef240b
Mahak Patel <mahakpateluiux@gmail.com>
2025-03-18 13:54:20 -0400

    Submitted version.

---
+++ Week 8/JS-LAB-8-DateMathTimers-25w/lab8-1-countdown.js
@@ -0,0 +1,47 @@
+﻿/* LAB 8-1 - FINAL COUNTDOWN!! */
+
+
+//create page load listener
+window.onload = function (){
+
+
+//create page load function
+
+	//create variables for required HTML elements
+	var todayData = document.getElementById("todayData");
+	var finalData = document.getElementById("finalData");
+	var dueData = document.getElementById("dueData");
+	var countMsg = document.getElementById("countMsg");
+	
+	// Create variables for now date and due date
+	var now = new Date();
+	var dueDate = new Date("April 22, 2024 18:00:00");
+	 
+	//create variables for now date and due date
+	
+	//OUTPUT NOW DATE & DUE DATE TO HTML PAGE
+
+	todayData.innerHTML = now.toDateString();
+    finalData.innerHTML = dueDate.toDateString();
+
+	//CONVERT TO UTC AND SUBTRACT TO GET TIME DIFFERENCE
+
+	var timeDifference = dueDate.getTime() - now.getTime();
+
+	//CONVERT TIME DIFFERENCE TO WHOLE NUMBER OF DAYS
+
+	var daysRemaining = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+
+	//LOGIC TO CHECK IF DUE DATE HAS PASSED, AND OUPUT APPROPRIATE MESSAGE TO HTML PAGE
+
+	if (daysRemaining > 0) {
+       
+        dueData.innerHTML = daysRemaining;
+    } else {
+       
+        countMsg.innerHTML = "The Deadline for the Final Assignment has passed!";
+        countMsg.style.backgroundColor = "#FF0000";
+        countMsg.style.color = "#FFFFFF";
+    }
+
+};
---
+++ Week 8/JS-LAB-8-DateMathTimers-25w/lab8-2-clock.js
@@ -0,0 +1,64 @@
+﻿/* LAB 8.2 - STOP TIME */
+
+
+//create page load listener
+window.onload = function (){
+
+//create page load function
+
+	//create variables for required HTML elements
+	var hoursOut = document.getElementById("hoursOut");
+    var minsOut = document.getElementById("minsOut");
+    var secsOut = document.getElementById("secsOut");
+    var btnStart = document.getElementById("btnStart");
+    var btnStop = document.getElementById("btnStop");
+	
+	//create time variable so all functions have access to it
+	var interval;
+	
+	//CREATE FUNCTION THAT DISPLAYS THE TIME
+	function displayTime() {
+       
+        var now = new Date();
+        var hours = now.getHours();
+        var minutes = now.getMinutes();
+        var seconds = now.getSeconds();
+
+        hours = formatTime(hours);
+        minutes = formatTime(minutes);
+        seconds = formatTime(seconds);
+
+        hoursOut.innerHTML = hours;
+        minsOut.innerHTML = ":" + minutes;
+        secsOut.innerHTML = ":" + seconds;
+    }
+
+    // CREATE FUNCTION TO FORMAT TIME (STRETCH GOAL)
+
+    function formatTime(time) {
+		if (time < 10) {
+			return "0" + time;
+		}
+		return time;
+    }
+	
+	//CREATE FUNCTION TO START THE CLOCK.
+	function startClock() {
+        if (interval) {
+            clearInterval(interval);
+        }
+
+        displayTime();
+
+        interval = setInterval(displayTime, 1000);
+    }
+	
+	//CREATE FUNCTION TO STOP THE CLOCK
+	function stopClock() {
+        clearInterval(interval);
+    }
+	
+	// SET EVENT LISTENERS
+	btnStart.addEventListener("click", startClock);
+    btnStop.addEventListener("click", stopClock); 
+};
